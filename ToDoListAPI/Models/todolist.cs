1383bf3cda5c5437f8daf87e7a20a2951a294849
Germeen George <114495626+germeengeorge@users.noreply.github.com>
2025-04-19 03:00:35 +0200

    Update todolist.cs

--- ToDoListAPI/Models/todolist.cs
+++ ToDoListAPI/Models/todolist.cs
@@ -1,14 +1,22 @@
-﻿using Microsoft.VisualBasic;
+using Microsoft.VisualBasic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ToDoListAPI.Models
 {
     public class todolist
     {
+        [Required]
         public int id {  get; set; }
+        [Required]
+        [StringLength(100,ErrorMessage = "Title can't be longer than 100 characters")]
         public string title {  get; set; }
         public string? description { get; set; }
+        [DataType(DataType.Date)]
         public DateTime DueDate { get; set; }
+        [Required]
         public bool IsCompleted {  get; set; }
+        [Required]
+        [RegularExpression("^(?i)(Low|Medium|High)$", ErrorMessage = "Priority must be Low, Medium, or High.")]
         public string Priority {  get; set; }
     }
 }
